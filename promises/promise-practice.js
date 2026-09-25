06405c4a9438e785bcc57ad852eff600d346598c
Pradeep Kumar <pk6949137@gmail.com>
2026-09-25 23:51:01 +0530

    more questions on promises

--- promises/promise-practice.js
+++ promises/promise-practice.js
@@ -146,6 +146,7 @@ oject()
   }) 
 
 
+
 /**
  * Promise.all Practice
 
@@ -157,4 +158,29 @@ oject()
  */
 
 
-  
+const promise1 = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("Promise 1 resolved")
+  }, 1000)
+})
+
+const promise2 = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("Promise 2 resolved")
+  }, 2000)
+})
+
+const promise3 = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("Promise 3 resolved")
+  }, 3000)
+})
+
+
+Promise.all([promise1, promise2, promise3])
+  .then((result) =>{
+    console.log(result)
+  })
+  .catch((err) =>{
+    console.log(err)
+  })
